d094a3c6ea642f275a8a88856231943fdcc621c2
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-24 03:56:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find {|i| i.name == item}
+      binding.pry
       resp.write "#{item.price}"
     else
       resp.write "Route not found"
